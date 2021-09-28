2865ed38e2e4dbcda3f766c456833c230ea07475
RohiniJ788 <72988330+RohiniJ788@users.noreply.github.com>
2021-09-28 20:54:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -14,4 +14,23 @@ recognition.onresult=function run(event){
     var content=event.results[0][0].transcript;
     console.log(content);
     document.getElementById("textbox").innerHTML=content;
+    speak();
+    Webcam.attach(camera);
 }
+
+function speak(){
+    var synth=window.speechSynthesis;
+    speak_data=document.getElementById("textbox").value;
+
+    var utterThis=new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+}
+
+Webcam.set({
+    width:320,
+    height:250,
+    image_format:'png',
+    png_quality:90
+});
+
+camera=document.getElementById("camera");
